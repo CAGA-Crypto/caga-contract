0a7a2ba3f5823d2b969165edc619f3d5b41254db
gsgoh <gsgoh92@gmail.com>
2023-10-04 22:24:15 +0800

    Add gov payment contract

---
+++ contracts/gov/Payment.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Payment is Ownable {
+    address public paymentToken;
+    address public treasuryWallet;
+    uint256 public price = 0;
+    
+    mapping(address => uint256) private credits;
+
+    event BoughtCredits(address indexed user, uint256 amount);
+    
+    constructor(address _paymentToken) {
+        paymentToken = _paymentToken;
+    }
+    
+    function payProposal(uint256 amount) external {
+        require(amount > 0, "Cannot purchase 0");
+        uint256 paymentPrice = amount * price;
+
+        IERC20 token = IERC20(paymentToken);
+        require(token.transferFrom(msg.sender, treasuryWallet, paymentPrice), "Transfer failed!");
+
+        credits[msg.sender] += 1;
+
+        emit BoughtCredits(msg.sender, amount);
+    }
+
+    function getAvailableCredits(address walletAddress) external view returns (uint256) {
+        return credits[walletAddress];
+    }
+    
+    function updateERC20Address(address newAddress) external onlyOwner {
+        paymentToken = newAddress;
+    }
+    
+    function updateTreasuryWallet(address newWallet) external onlyOwner {
+        treasuryWallet = newWallet;
+    }
+    
+    function setPrice(uint256 newPrice) external onlyOwner {
+        price = newPrice;
+    }
+}
