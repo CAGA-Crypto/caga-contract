e92827a7992e9ecb47e05e75cb80fe4891cdc95d
glacvorx <rongheng_1@hotmail.com>
2023-10-09 01:05:35 +0800

    Updated gov token contract.

--- contracts/governance/gov_token.sol
+++ contracts/governance/gov_token.sol
@@ -8,34 +8,19 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract Gov_Token is Initializable, ERC20Upgradeable, ERC20PermitUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
-	address public protocol;
-
 	/// @custom:oz-upgrades-unsafe-allow constructor
 	constructor() {
 		_disableInitializers();
 	}
 
-	function initialize(address protocol_) public initializer {
+	function initialize() public initializer {
 		__ERC20_init("Crypto Asset Governance Alliance", "CAGA");
 		__ERC20Permit_init("Crypto Asset Governance Alliance");
 		__Ownable_init();
 		__UUPSUpgradeable_init();
 
 		_mint(msg.sender, 1000000 * 10 ** decimals());
-
-		protocol = protocol_;
 	}
 
 	function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
-
-	modifier onlyProtocol() {
-		require(_msgSender() == protocol, "caller is not the protocol");
-		_;
-	}
-
-	function set_protocol(address new_protocol) external onlyOwner {
-		require(new_protocol != address(0), "address cannot be 0");
-
-		protocol = new_protocol;
-	}
 }
