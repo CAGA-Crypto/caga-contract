3e9cafa8d3382ced4114b45a1d058befdd4141b3
glacvorx <rongheng_1@hotmail.com>
2023-10-09 00:35:47 +0800

    Updated solidity version for payment contract.

--- contracts/governance/MockToken.sol
+++ contracts/governance/MockToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.21;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockToken is ERC20 {
-    constructor() ERC20("TestToken", "TTK") {
-        _mint(msg.sender, 1000000 * 10 ** decimals());
-    }
+	constructor() ERC20("TestToken", "TTK") {
+		_mint(msg.sender, 1000000 * 10 ** decimals());
+	}
 }
--- contracts/governance/Payment.sol
+++ contracts/governance/Payment.sol
@@ -1,47 +1,47 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.21;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Payment is Ownable {
-    address public paymentToken;
-    address public treasuryWallet;
-    uint256 public price = 0;
-    
-    mapping(address => uint256) private credits;
+	address public paymentToken;
+	address public treasuryWallet;
+	uint256 public price = 0;
 
-    event BoughtCredits(address indexed user, uint256 amount);
-    
-    constructor(address _paymentToken) {
-        paymentToken = _paymentToken;
-    }
-    
-    function payProposal(uint256 amount) external {
-        require(amount > 0, "Cannot purchase 0");
-        uint256 paymentPrice = amount * price;
+	mapping(address => uint256) private credits;
 
-        IERC20 token = IERC20(paymentToken);
-        require(token.transferFrom(msg.sender, treasuryWallet, paymentPrice), "Transfer failed!");
+	event BoughtCredits(address indexed user, uint256 amount);
 
-        credits[msg.sender] += amount;
+	constructor(address _paymentToken) {
+		paymentToken = _paymentToken;
+	}
 
-        emit BoughtCredits(msg.sender, amount);
-    }
+	function payProposal(uint256 amount) external {
+		require(amount > 0, "Cannot purchase 0");
+		uint256 paymentPrice = amount * price;
 
-    function getAvailableCredits(address walletAddress) external view returns (uint256) {
-        return credits[walletAddress];
-    }
-    
-    function updateERC20Address(address newAddress) external onlyOwner {
-        paymentToken = newAddress;
-    }
-    
-    function updateTreasuryWallet(address newWallet) external onlyOwner {
-        treasuryWallet = newWallet;
-    }
-    
-    function setPrice(uint256 newPrice) external onlyOwner {
-        price = newPrice;
-    }
+		IERC20 token = IERC20(paymentToken);
+		require(token.transferFrom(msg.sender, treasuryWallet, paymentPrice), "Transfer failed!");
+
+		credits[msg.sender] += amount;
+
+		emit BoughtCredits(msg.sender, amount);
+	}
+
+	function getAvailableCredits(address walletAddress) external view returns (uint256) {
+		return credits[walletAddress];
+	}
+
+	function updateERC20Address(address newAddress) external onlyOwner {
+		paymentToken = newAddress;
+	}
+
+	function updateTreasuryWallet(address newWallet) external onlyOwner {
+		treasuryWallet = newWallet;
+	}
+
+	function setPrice(uint256 newPrice) external onlyOwner {
+		price = newPrice;
+	}
 }
