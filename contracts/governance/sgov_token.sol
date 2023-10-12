87f9bd061b94a477d6220e4b8385602d7aba84d0
glacvorx <rongheng_1@hotmail.com>
2023-10-13 03:21:48 +0800

    Removed initialise protocol addresses 2.

--- contracts/governance/sgov_token.sol
+++ contracts/governance/sgov_token.sol
@@ -18,14 +18,12 @@ contract sGov_Token is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable
 		_disableInitializers();
 	}
 
-	function initialize(address protocol_) public initializer {
+	function initialize() public initializer {
 		__ERC20_init("sCAGA", "sCAGA");
 		__ERC20Burnable_init();
 		__Ownable_init();
 		__ERC20Permit_init("sCAGA");
 		__UUPSUpgradeable_init();
-
-		protocol = protocol_;
 	}
 
 	function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
