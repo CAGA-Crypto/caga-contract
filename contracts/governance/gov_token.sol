2fa6bd2d22a3527ce78b856677bdc05a8f40f4d5
0xchamel <mauroliu77@gmail.com>
2023-10-20 08:23:09 +0800

    Added audited gov_token

--- contracts/governance/gov_token.sol
+++ contracts/governance/gov_token.sol
@@ -7,7 +7,14 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
+interface IAntisnipe {
+	function assureCanTransfer(address sender, address from, address to, uint256 amount) external;
+}
+
 contract Gov_Token is Initializable, ERC20Upgradeable, ERC20PermitUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+	IAntisnipe public antisnipe;
+	bool public antisnipeDisable;
+
 	/// @custom:oz-upgrades-unsafe-allow constructor
 	constructor() {
 		_disableInitializers();
@@ -23,4 +30,18 @@ contract Gov_Token is Initializable, ERC20Upgradeable, ERC20PermitUpgradeable, O
 	}
 
 	function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+	function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
+		if (from == address(0) || to == address(0)) return;
+		if (!antisnipeDisable && address(antisnipe) != address(0)) antisnipe.assureCanTransfer(msg.sender, from, to, amount);
+	}
+
+	function setAntisnipeDisable() external onlyOwner {
+		require(!antisnipeDisable);
+		antisnipeDisable = true;
+	}
+
+	function setAntisnipeAddress(address addr) external onlyOwner {
+		antisnipe = IAntisnipe(addr);
+	}
 }
