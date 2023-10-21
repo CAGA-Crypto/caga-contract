de54ba67559bf83cbf046960bfb2ef0a618ee305
glacvorx <rongheng_1@hotmail.com>
2023-10-22 03:57:39 +0800

    Updated core contract address for testnet.

--- scripts/set_protocol.js
+++ scripts/set_protocol.js
@@ -11,7 +11,7 @@ async function main() {
 	// Deploy Core contracts
 	const LS_Token_Proxy = await get_contract("LS_Token", "0xD0C3d30da906cB8e225CD6F79d1A4E40345B2397");
 	const Withdraw_Proxy = await get_contract("Withdraw", "0xB0C8edA3a3ab13Cd05D6d8332962F117602A4d4F");
-	const Core_Proxy = await get_contract("Core", "0x443FA96Ae5b815C52fF4056Fd560c78F123acbA7");
+	const Core_Proxy = await get_contract("Core", "0x13d5D2e633a33B9eD9D6e68F16B9cB93f17a8aDe");
 	const core_address = await Core_Proxy.getAddress();
 
 	// Configure protocol addresses for core contracts
--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -19,7 +19,7 @@ async function main() {
 	// Upgrade Core contracts
 	await upgrade_contract("0xD0C3d30da906cB8e225CD6F79d1A4E40345B2397", "LS_Token");
 	await upgrade_contract("0xB0C8edA3a3ab13Cd05D6d8332962F117602A4d4F", "Withdraw");
-	await upgrade_contract("0x443FA96Ae5b815C52fF4056Fd560c78F123acbA7", "Core");
+	await upgrade_contract("0x13d5D2e633a33B9eD9D6e68F16B9cB93f17a8aDe", "Core");
 
 	// Upgrade Governance contracts
 	await upgrade_contract("0x321272D3DF4234f487f8A4cA3Ea3AFF4c4FDBcA9", "Gov_Token");
