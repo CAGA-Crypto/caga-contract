795007ad36086b69d2d73958787299ff43c33c54
glacvorx <rongheng_1@hotmail.com>
2023-10-22 03:47:08 +0800

    Formatted payment test script.

--- tests/Payment.test.js
+++ tests/Payment.test.js
@@ -2,44 +2,47 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Payment", function () {
-  let Payment, MockToken, payment, mockToken, owner, addr1, addr2;
+	let Payment, PaymentToken, USDTToken, payment, paymentToken, usdtToken, owner, addr1, addr2;
 
-  beforeEach(async function () {
-    [owner, addr1, addr2] = await ethers.getSigners();
+	beforeEach(async function () {
+		[owner, addr1, addr2] = await ethers.getSigners();
 
-    MockToken = await ethers.getContractFactory("MockToken");
-    mockToken = await MockToken.deploy();
+		PaymentToken = await ethers.getContractFactory("MockToken");
+		paymentToken = await PaymentToken.deploy();
 
-    Payment = await ethers.getContractFactory("Payment");
-    payment = await Payment.deploy(mockToken.address);
-  });
+		USDTToken = await ethers.getContractFactory("MockToken");
+		usdtToken = await USDTToken.deploy();
 
-  it("Should set the correct treasury address", async function () {
-    await payment.updateTreasuryWallet(addr2.address);
-    expect(await payment.treasuryWallet()).to.equal(addr2.address);
-  });
+		Payment = await ethers.getContractFactory("Payment");
+		payment = await Payment.deploy(paymentToken.address, usdtToken.address);
+	});
 
-  it("Should make a payment correctly", async function () {
-    await payment.setPrice(ethers.utils.parseEther("1"));
-    await payment.updateTreasuryWallet(addr2.address);
+	it("Should set the correct treasury address", async function () {
+		await payment.updateTreasuryWallet(addr2.address);
+		expect(await payment.treasuryWallet()).to.equal(addr2.address);
+	});
 
-    await mockToken.approve(payment.address, ethers.utils.parseEther("5"));
-    await payment.payProposal(5);
+	it("Should make a payment correctly", async function () {
+		await payment.setPrice(ethers.utils.parseEther("1"));
+		await payment.updateTreasuryWallet(addr2.address);
 
-    expect(await mockToken.balanceOf(addr2.address)).to.equal(ethers.utils.parseEther("5"));
-    expect(await payment.getAvailableCredits(owner.address)).to.equal(5);
-  });
+		await paymentToken.approve(payment.address, ethers.utils.parseEther("5"));
+		await payment.payProposal(5);
 
-  it("Should update the price correctly", async function () {
-    await payment.setPrice(ethers.utils.parseEther("2"));
-    expect(await payment.price()).to.equal(ethers.utils.parseEther("2"));
-  });
+		expect(await paymentToken.balanceOf(addr2.address)).to.equal(ethers.utils.parseEther("5"));
+		expect(await payment.getAvailableCredits(owner.address)).to.equal(5);
+	});
 
-  it("Should update the token address correctly", async function () {
-    const AnotherMockToken = await ethers.getContractFactory("MockToken");
-    const anotherMockToken = await AnotherMockToken.deploy();
+	it("Should update the price correctly", async function () {
+		await payment.setPrice(ethers.utils.parseEther("2"));
+		expect(await payment.price()).to.equal(ethers.utils.parseEther("2"));
+	});
 
-    await payment.updateERC20Address(anotherMockToken.address);
-    expect(await payment.paymentToken()).to.equal(anotherMockToken.address);
-  });
+	it("Should update the token address correctly", async function () {
+		const AnotherMockToken = await ethers.getContractFactory("PaymentToken");
+		const anotherMockToken = await AnotherMockToken.deploy();
+
+		await payment.updateERC20Address(anotherMockToken.address);
+		expect(await payment.paymentToken()).to.equal(anotherMockToken.address);
+	});
 });
