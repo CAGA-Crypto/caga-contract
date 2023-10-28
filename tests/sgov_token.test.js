dad3218b40c3f4039d121a3bdf16902426a92169
glacvorx <rongheng_1@hotmail.com>
2023-10-28 17:59:59 +0800

    Updated sgov_token tests.

--- tests/sgov_token.test.js
+++ tests/sgov_token.test.js
@@ -3,18 +3,29 @@ const { expect } = require("chai");
 
 describe("sGov_Token", function () {
 	let sGov_Token;
+	let sGov_Token2;
 	let sgov_token;
+	let sgov_token_address;
 	let owner;
 	let protocol;
 
 	beforeEach(async function () {
 		sGov_Token = await ethers.getContractFactory("sGov_Token");
+		sGov_Token2 = await ethers.getContractFactory("sGov_Token");
 
 		[owner, protocol, account3] = await ethers.getSigners();
-		sgov_token = await upgrades.deployProxy(sGov_Token, [], { kind: "uups" });
+		sgov_token = await upgrades.deployProxy(sGov_Token, [], { kind: "uups", redeployImplementation: "always" });
 		await sgov_token.waitForDeployment();
 
+		sgov_token_address = await sgov_token.getAddress();
+
 		await sgov_token.set_protocol(protocol.address);
+	});
+
+	describe("initialize", function () {
+		it("should set the protocol address", async function () {
+			expect(await sgov_token.protocol()).to.equal(protocol.address);
+		});
 	});
 
 	describe("set_protocol", function () {
@@ -29,6 +40,18 @@ describe("sGov_Token", function () {
 
 		it("should revert if called by a non-owner", async function () {
 			await expect(sgov_token.connect(account3).set_protocol(owner.address)).to.be.revertedWith("Ownable: caller is not the owner");
+		});
+	});
+
+	describe("upgrade", function () {
+		it("should upgrade the contract", async function () {
+			const implementationAddress = await upgrades.erc1967.getImplementationAddress(sgov_token_address);
+
+			const Proxy = await upgrades.upgradeProxy(sgov_token_address, sGov_Token2);
+			await Proxy.waitForDeployment();
+
+			const implementationAddress2 = await upgrades.erc1967.getImplementationAddress(sgov_token_address);
+			expect(implementationAddress).to.not.equal(implementationAddress2);
 		});
 	});
 
